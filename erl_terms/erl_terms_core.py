e727af8ea469e31c3907498245361467ada79584
Renat Idrisov <ridrisov@mz.com>
2017-05-01 19:25:51 +0200

    Better comment parsing

--- erl_terms/erl_terms_core.py
+++ erl_terms/erl_terms_core.py
@@ -19,7 +19,7 @@ def transform(ast):
         return [ast.text=="true"]
     elif ast.expr_name == "string":
         return [ast.text[1:-1].replace(r'\"', '"').replace(r'\\\\', '\\\\')]
-    elif ast.expr_name == "atom": 
+    elif ast.expr_name == "atom":
         return [ast.text]
     else:
         lis = reduce(lambda a, x: a + transform(x), ast.children, [])
@@ -37,7 +37,7 @@ def lex(text):
     entry = (term _ "." _)* _
     term = boolean / atom / list / tuple / map / string / binary / number
     atom = ~"[a-z][0-9a-zA-Z_]*" / ("'" ~"[^']*" "'")
-    _ = ~"\s*" 
+    _ = ~"\s*" (~"%[^\\r\\n]*\s*")*
     list = ( _ "[" _ term (_ "," _ term)* _ "]" ) / ( _ "[" _ "]")
     tuple = ( _ "{" _ term (_ "," _ term)* _ "}" ) / ( _ "{" _ "}")
     map   = ( _ "#{" _ keyvalue (_ "," _ keyvalue)* _ "}" ) / ( _ "#{" _ "}")
@@ -47,9 +47,8 @@ def lex(text):
     boolean = "true" / "false"
     number = ~"\-?[0-9]+\#[0-9a-zA-Z]+" / ~"\-?[0-9]+(\.[0-9]+)?((e|E)(\-|\+)?[0-9]+)?"
     """)
-    nocomments = re.sub("(?m)%.*?$", "", text)
     try:
-        return grammar.parse(nocomments)
+        return grammar.parse(text)
     except parsimonious.exceptions.ParseError as e:
         raise ParseError(e)
 
